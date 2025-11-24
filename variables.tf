6f41db13bd7b2b6105c95715a716779f757bf340
andre <andrepatete@gmail.com>
2025-11-24 08:13:26 -0300

    feat: cria VPCs e Subnets Publicas e Prvadas

---
+++ variables.tf
@@ -0,0 +1,45 @@
+variable "project_name" {
+  type = string
+}
+
+variable "region" {
+  type = string
+}
+
+variable "vpc_cidr" {
+  type        = string
+  description = "CIDR principal da VPC"
+}
+
+variable "vpc_additional_cidrs" {
+  type        = list(string)
+  description = "Lista de CIDRs adicionais da VPC"
+  default     = []
+}
+variable "public_subnets" {
+  description = "Lista de public subnets da VPC"
+  type = list(object({
+    name              = string
+    cidr              = string
+    availability_zone = string
+  }))
+}
+
+variable "private_subnets" {
+  description = "Lista de private subnets da VPC"
+  type = list(object({
+    name              = string
+    cidr              = string
+    availability_zone = string
+  }))
+}
+
+variable "database_subnets" {
+  description = "Lista de database subnets da VPC"
+  default     = []
+  type = list(object({
+    name              = string
+    cidr              = string
+    availability_zone = string
+  }))
+}
